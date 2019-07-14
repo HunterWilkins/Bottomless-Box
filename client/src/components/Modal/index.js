5e87d93a58135cad87ca1adaab43102ef3fe9ee2
HunterWilkins <wilkins.hunter@gmail.com>
2019-07-14 12:46:36 -0500

    Added Drinks pocket

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -14,6 +14,7 @@ function Modal(props) {
                 <Pocket func = "modal" name = "Fruit"  makePocket = {props.makePocket}/>
                 <Pocket func = "modal" name = "Vegetables" makePocket = {props.makePocket} />
                 <Pocket func = "modal" name = "Junk Food" makePocket = {props.makePocket} />
+                <Pocket func = "modal" name = "Drinks" makePocket = {props.makePocket} />
                 <Pocket func = "modal" name = "Medicine" makePocket = {props.makePocket} />
                 <Pocket func = "modal" name = "Electronics" makePocket = {props.makePocket} />
                 <Pocket func = "modal" name = "Misc" makePocket = {props.makePocket} />
